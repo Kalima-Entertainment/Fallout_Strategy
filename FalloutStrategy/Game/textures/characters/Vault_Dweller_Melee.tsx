bf1b14856c20d660cd818a9d256c8113ed9bcdf1
marcpages2020 <47534017+marcpages2020@users.noreply.github.com>
2020-02-26 09:19:58 +0100

    editing animations

---
+++ FalloutStrategy/Game/textures/characters/Vault_Dweller_Melee.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="Vault_Dweller_Melee" tilewidth="64" tileheight="84" tilecount="768" columns="32">
+ <image source="Vault_Dweller_Melee.png" width="2048" height="2048"/>
+</tileset>
