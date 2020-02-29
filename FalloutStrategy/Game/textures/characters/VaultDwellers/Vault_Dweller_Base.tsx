2c45eec685de17fe59a1af5005c9ab9f68a9cf6e
marcpages2020 <47534017+marcpages2020@users.noreply.github.com>
2020-02-29 16:51:27 +0100

    changes from master

---
+++ FalloutStrategy/Game/textures/characters/VaultDwellers/Vault_Dweller_Base.tsx
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Vault_Dweller_Base" tilewidth="256" tileheight="256" tilecount="64" columns="8">
+ <image source="../../../../../../../../Desktop/Vault_Dweller_Base.png" width="2048" height="2048"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="16" duration="100"/>
+   <frame tileid="17" duration="100"/>
+   <frame tileid="18" duration="100"/>
+   <frame tileid="19" duration="100"/>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="12" duration="100"/>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="14" duration="100"/>
+   <frame tileid="15" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="24">
+  <animation>
+   <frame tileid="24" duration="100"/>
+   <frame tileid="25" duration="100"/>
+   <frame tileid="26" duration="100"/>
+   <frame tileid="27" duration="100"/>
+   <frame tileid="28" duration="100"/>
+   <frame tileid="29" duration="100"/>
+   <frame tileid="30" duration="100"/>
+   <frame tileid="31" duration="100"/>
+   <frame tileid="32" duration="100"/>
+   <frame tileid="33" duration="100"/>
+   <frame tileid="34" duration="100"/>
+   <frame tileid="35" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="40">
+  <animation>
+   <frame tileid="40" duration="100"/>
+   <frame tileid="41" duration="100"/>
+   <frame tileid="42" duration="100"/>
+   <frame tileid="43" duration="100"/>
+   <frame tileid="44" duration="100"/>
+   <frame tileid="48" duration="100"/>
+   <frame tileid="49" duration="100"/>
+   <frame tileid="50" duration="100"/>
+   <frame tileid="51" duration="100"/>
+   <frame tileid="52" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
