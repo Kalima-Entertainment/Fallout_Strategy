1c484d862fd4d9407ecc86079e8d96a2e5283814
maciadalmau <47743853+maciadalmau@users.noreply.github.com>
2020-04-18 19:54:13 +0200

    Changing font size option

--- FalloutStrategy/Motor2D/j1Fonts.h
+++ FalloutStrategy/Motor2D/j1Fonts.h
@@ -36,7 +36,7 @@ public:
 	_TTF_Font* const Load(const char* path, int size = 12);
 
 	// Create a surface from text
-	SDL_Texture* Print(const char* text, SDL_Color color = { 255, 255, 255, 255 }, std::string font_name = ("No_font"));
+	SDL_Texture* Print(const char* text, SDL_Color color = { 255, 255, 255, 255 }, std::string font_name = ("No_font"), int size = 38);
 
 	bool CalcSize(const char* text, int& width, int& height, _TTF_Font* font = NULL) const;
 
