930417b898539c0df663a64481379fc3e640372b
silvino00 <47743896+silvino00@users.noreply.github.com>
2020-05-14 02:29:54 +0200

    Tutorial screen reorganized

--- FalloutStrategy/Motor2D/MenuManager.h
+++ FalloutStrategy/Motor2D/MenuManager.h
@@ -73,7 +73,7 @@ public:
 	UI_element* select_faction_photos[4];
 	UI_element* pause_menu[7];
 	UI_element* gui_ingame[4];
-	UI_element* tutorial[6];
+	UI_element* tutorial[8];
 	UI_Label*	timer_item;
 	UI_Label*	timer_minutes;
 	UI_element* resources[3];
