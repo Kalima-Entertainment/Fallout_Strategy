c98035c9da483a0e4eb4acafdfe43064497649a8
marcpages2020 <47534017+marcpages2020@users.noreply.github.com>
2020-06-09 22:23:09 +0200

    entities not getting stuck if origin is not walkable

--- FalloutStrategy/FalloutEngine/DynamicEntity.cpp
+++ FalloutStrategy/FalloutEngine/DynamicEntity.cpp
@@ -177,18 +177,17 @@ bool DynamicEntity::PathfindToPosition(iPoint destination) {
 		return ret;
 
 	if (!App->pathfinding->IsWalkable(current_tile)) {
-		destination = App->pathfinding->FindWalkableAdjacentTile(current_tile);
-		if(!App->pathfinding->IsWalkable(destination))
-			LOG("unwalkable origin");
-		ret = false;
+		next_tile = App->pathfinding->FindNearestWalkableTile(current_tile, destination);
+		ret = App->pathfinding->IsWalkable(next_tile);
 	}
 
 	//if the tile is in the map but it's not walkable
 	if (!App->pathfinding->IsWalkable(destination)) {
 		destination = App->pathfinding->FindNearestWalkableTile(current_tile, destination);
-		if (!App->pathfinding->IsWalkable(destination))
+		ret = App->pathfinding->IsWalkable(destination);
+
+		if(!ret)
 			LOG("unwalkable destination");
-		ret = false;
 	}
 
 	if (App->entities->occupied_tiles[destination.x][destination.y]) {
@@ -304,7 +303,9 @@ void DynamicEntity::Move(float dt) {
 				//if there is a new path erase the first element
 				if (path_to_target.size() > 0)
 					path_to_target.erase(path_to_target.cbegin());
-
+			}
+			else {
+				PathfindToPosition(target_tile);
 			}
 		}
 
