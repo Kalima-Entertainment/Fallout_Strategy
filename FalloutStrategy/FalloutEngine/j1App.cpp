fe95cbf9bf8c146f3847600fa54709b476d70427
Pablo Galve <eduinvestor100@gmail.com>
2020-06-14 12:56:51 +0200

    comment crashing code

--- FalloutStrategy/FalloutEngine/j1App.cpp
+++ FalloutStrategy/FalloutEngine/j1App.cpp
@@ -270,11 +270,11 @@ void j1App::FinishUpdate()
 	uint32 last_frame_ms = frame_time.Read();
 	uint32 frames_on_last_update = prev_last_sec_frame_count;
 	static char title[256];
-
-	sprintf_s(title, 256, " Fallout Strategy 0.5 - Kalima Entertainment | FPS: %d | Last dt: %.3f | Camera X: %i Camera Y: %i",
+	
+	//sprintf_s(title, 256, " Fallout Strategy 0.5 - Kalima Entertainment | FPS: %d | Last dt: %.3f | Camera X: %i Camera Y: %i",
 	sprintf_s(title, 256, " Fallout Strategy 0.9.5 - Kalima Entertainment | FPS: %d",
-		prev_last_sec_frame_count);
-	App->win->SetTitle(title);
+		prev_last_sec_frame_count, dt, App->render->camera.x, App->render->camera.y);
+	App->win->SetTitle(title);	
 
 	if(capped_ms > 0 && last_frame_ms < capped_ms)
 	{
